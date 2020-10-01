c554f649ec91f60016002fe0fa9b6ebd26d71a79
mrwhoknows55 <avdhuttanugade55@gmail.com>
2020-10-01 23:38:56 +0530

    add: collapse ⌨ when clicked outside input fun

--- app/src/main/java/com/mrwhoknows/util_fun/MainActivity.kt
+++ app/src/main/java/com/mrwhoknows/util_fun/MainActivity.kt
@@ -2,10 +2,18 @@ package com.mrwhoknows.util_fun
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
+import com.mrwhoknows.util_fun.util.collapseKeyboardOnOutsideTap
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    //    Collapse the keyboard when the user taps outside the input area
+    override fun dispatchTouchEvent(motionEvent: MotionEvent): Boolean {
+        collapseKeyboardOnOutsideTap(this, motionEvent, currentFocus)
+        return super.dispatchTouchEvent(motionEvent)
     }
 }
